73178778cc002509ea9f644fd500394a26a93e7c
PratikGarai <pratikgarai0208@gmail.com>
2024-04-06 23:14:57 +0530

    added variant scaling

--- lister.py
+++ lister.py
@@ -12,13 +12,16 @@ if __name__ == "__main__":
                     help="Root folder to start exploring from")
     ap.add_argument("-i", "--ignore", nargs="+",
                     help="List of directories to ignore")
-    ap.add_argument("-s", "--scale", type=int, default=1,
-                    help="Scale of the graph")
+    ap.add_argument("-sv", "--scalev", type=float, default=1.0,
+                    help="Vertical sale of the graph")
+    ap.add_argument("-sh", "--scaleh", type=float, default=1.0,
+                    help="Horizontal sale of the graph")
     args = vars(ap.parse_args())
 
     root = args["root"]
     ignore_list = args["ignore"]
-    scale = args["scale"]
+    scalev = args["scalev"]
+    scaleh = args["scaleh"]
 
     if ignore_list is None:
         ignore_list = []
@@ -26,7 +29,7 @@ if __name__ == "__main__":
     grapher = Grapher(root_folder=root, ignore_list=ignore_list)
 
     graph = grapher.get_graph()
-    plt.figure(figsize=[6*scale, 6*scale])
+    plt.figure(figsize=[6*scaleh, 6*scalev])
     pos = nx.nx_agraph.graphviz_layout(graph, prog="dot")
     nx.draw(graph, pos, with_labels=True, node_shape="o")
     plt.savefig("output_graph.png", bbox_inches='tight', dpi=100)
