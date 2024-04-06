aa8878201e5f23ee1f46b950d9fd3381005a52fd
PratikGarai <pratikgarai0208@gmail.com>
2024-04-06 21:10:06 +0530

    argparse

--- lister.py
+++ lister.py
@@ -1,16 +1,26 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 from networkx.drawing.nx_pydot import graphviz_layout
+from argparse import ArgumentParser
 
 from src.grapher import Grapher
 
 if __name__ == "__main__":
-    root = "sample"
-    ignore_list = ["ignore"]
+
+    ap = ArgumentParser()
+    ap.add_argument("-r", "--root", required=True, help="Root folder to start exploring from")
+    ap.add_argument("-i", "--ignore", nargs="+", help="List of directories to ignore")
+    args = vars(ap.parse_args())
+
+    root = args["root"]
+    ignore_list = args["ignore"]
+
+    if ignore_list is None:
+        ignore_list = []
 
     grapher = Grapher(root_folder=root, ignore_list=ignore_list)
 
     graph = grapher.get_graph()
     pos = graphviz_layout(graph, prog="dot")
     nx.draw(graph, pos, with_labels=True, node_shape="o")
-    plt.savefig("image.png",bbox_inches='tight',dpi=100)
+    plt.savefig("output_graph.png",bbox_inches='tight',dpi=100)
